112dd8518b9cb76c35e63e2b21221ded2b0eb2d7
dikshachawla9397 <79681152+dikshachawla9397@users.noreply.github.com>
2021-09-08 11:39:43 -0400

    Create Login.php

---
+++ application/controllers/Login.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+		if ($this->form_validation->run() == FALSE)
+        {
+			//error
+			$this->load->view('login');
+		}
+		else
+		{
+			
+			$email=$this->input->post('email');
+			$password=$this->input->post('password');
+            $this->db->from('user');
+			$this->db->select('*');
+            $this->db->where('email', $email);
+            $this->db->where('password', $password);
+            $query = $this->db->get();
+            $result=$query->result();
+            if($result)
+			{
+				if($result[0]->status=="UNAPPROVED")
+				{
+					$this->session->set_flashdata('Message', 'Your Account is Block');
+					$this->load->view('login');
+				}
+				else
+				{
+					$this->session->set_userdata($result);
+					$this->session->set_userdata('User', $result[0]->id);
+					redirect(base_url());
+				}
+				
+				
+				
+			}
+			else if($email=="admin@gmail.com" && $password=="admin")
+			{
+					$this->session->set_userdata('Admin', 'Admin');
+					redirect(base_url().'Dashboardadmin');
+			}
+			else
+			{
+				$this->session->set_flashdata('Message', 'Email Id or Password Incorrect');
+				$this->load->view('login');
+				
+			}
+
+			
+		}
+	}
+}
